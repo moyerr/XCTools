ebe1489a1d0a8bec16550a050853d597cc9491dc
Robbie Moyer <rmoyer08@gmail.com>
2023-10-19 17:03:23 -0400

    Add support for -testPlan, -skipPackagePluginValidation, -skipMacroValidation

--- Sources/XCTools/Xcodebuild/Xcodebuild.test.swift
+++ Sources/XCTools/Xcodebuild/Xcodebuild.test.swift
@@ -1,7 +1,18 @@
 import Sh
 
 extension Xcodebuild {
-  public func test(_ sink: Sink = .terminal) async throws {
-    try await sh(sink, serializedCommand(action: "test"))
-  }
+    public func test(
+        _ sink: Sink = .terminal,
+        testPlan: String? = nil
+    ) async throws {
+        var action = ""
+
+        if let testPlan {
+            action.append("-testPlan \(testPlan) ")
+        }
+
+        action.append("test")
+
+        try await sh(sink, serializedCommand(action: action))
+    }
 }
--- Sources/XCTools/Xcodebuild/Xcodebuild.testWithoutBuilding.swift
+++ Sources/XCTools/Xcodebuild/Xcodebuild.testWithoutBuilding.swift
@@ -1,15 +1,27 @@
 import Sh
 
 extension Xcodebuild {
-  /// Test compiled bundles. If a `scheme` is provided then
-  /// the command finds bundles in the build root (SRCROOT).
-  /// If an xctestrun file is provided then the command finds
-  /// bundles at paths specified in the xctestrun file.
-  public func testWithoutBuilding(_ sink: Sink = .terminal, xctestrun: String? = nil) async throws {
-    if let xctestrun = xctestrun {
-      try await sh(sink, serializedCommand(action: "-xctestrun \(xctestrun) test-without-building"))
-    } else {
-      try await sh(sink, serializedCommand(action: "test-without-building"))
+    /// Test compiled bundles. If a `scheme` is provided then
+    /// the command finds bundles in the build root (SRCROOT).
+    /// If an xctestrun file is provided then the command finds
+    /// bundles at paths specified in the xctestrun file.
+    public func testWithoutBuilding(
+        _ sink: Sink = .terminal,
+        testPlan: String? = nil,
+        xctestrun: String? = nil
+    ) async throws {
+        var action = ""
+
+        if let xctestrun {
+            action.append("-xctestrun \(xctestrun) ")
+        }
+
+        if let testPlan {
+            action.append("-testPlan \(testPlan) ")
+        }
+
+        action.append("test-without-building")
+
+        try await sh(sink, serializedCommand(action: action))
     }
-  }
 }
